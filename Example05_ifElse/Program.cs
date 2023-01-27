d17f3eba350004323821f3163759c1206758ea03
Yanina <moonspb75@mail.ru>
2023-01-27 14:57:16 +0300

    Написала задачу с if or Else

---
+++ Example05_ifElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "мария")
+{
+    Console.WriteLine("Ура, это же МАРИЯ!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
